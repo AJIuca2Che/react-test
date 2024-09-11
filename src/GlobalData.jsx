acdc1b1850411e0f639f9744d0bbc347e5a94e7c
AJIuca2Che <danila.v.sokolov38@gmail.com>
2024-09-11 20:45:46 +0300

    #

---
+++ src/GlobalData.jsx
@@ -0,0 +1,25 @@
+import Table from 'react-bootstrap/Table';
+
+function GlobalData() {
+  return (
+    <Table>
+      
+      <tbody>
+        <tr>
+          <td>BTC Dominance</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Vol 24h</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>MarcetCap</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+}
+
+export default GlobalData;
