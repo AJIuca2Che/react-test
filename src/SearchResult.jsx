8208607a334ab8a97da392d139cc1c412a97ca11
AJIuca2Che <danila.v.sokolov38@gmail.com>
2024-09-13 18:30:03 +0300

    Search Result Table

---
+++ src/SearchResult.jsx
@@ -0,0 +1,23 @@
+import Coins from "./Coins";
+import Table from 'react-bootstrap/Table';
+function SearchResult () {
+    return (
+        <>
+        <h3>Search Result</h3>
+        <Table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Logo</th>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+            
+        </tbody>
+        </Table>
+    </>
+    );
+}
+
+export default SearchResult;
