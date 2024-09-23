d45c1c9618eaefcc97211a6bd7ac7dee309f354c
AJIuca2Che <danila.v.sokolov38@gmail.com>
2024-09-23 20:42:16 +0300

    lesson 5

--- src/Body/GlobalData.jsx
+++ src/Body/GlobalData.jsx
@@ -1,21 +1,28 @@
-import Table from 'react-bootstrap/Table';
+import React from "react";
+import Table from "react-bootstrap/Table";
+import { getGlobalData } from "../services/api";
 
 function GlobalData() {
+  const [globalData, setGlobalData] = React.useState({});
+
+  React.useEffect(() => {
+    getGlobalData().then(setGlobalData);
+  }, []);
+
   return (
     <Table>
-      
       <tbody>
         <tr>
           <td>BTC Dominance</td>
-          <td></td>
+          <td>{globalData.bitcoin_dominance_percentage} %</td>
         </tr>
         <tr>
           <td>Vol 24h</td>
-          <td></td>
+          <td>{globalData.volume_24h_usd}</td>
         </tr>
         <tr>
-          <td>MarcetCap</td>
-          <td></td>
+          <td>Market Cap</td>
+          <td>{globalData.market_cap_usd}</td>
         </tr>
       </tbody>
     </Table>
